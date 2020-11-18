4e26f2c34fe624a7ba67cb2f792aa971c502f487
ezio341 <argadiaz09@gmail.com>
2020-11-18 21:08:32 +0700

    add dist

--- src/javathread/JavaThread.java
+++ src/javathread/JavaThread.java
@@ -5,6 +5,8 @@
  */
 package javathread;
 
+import tugas.TimeUI;
+
 /**
  *
  * @author ASUS
@@ -16,6 +18,7 @@ public class JavaThread {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        TimeUI.main(args);
     }
     
 }
